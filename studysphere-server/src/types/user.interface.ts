c3a86e25a9943b5bf6fb46399fa27593854e9c8f
Aidan Ouckama <aidan@moonshot-ai.com>
2026-03-28 00:12:30 -0400

    express request body with user (this is prob wrong but im tired)

--- studysphere-server/src/types/user.interface.ts
+++ studysphere-server/src/types/user.interface.ts
@@ -1,5 +1,13 @@
 export type UserId = string;
 
+declare global {
+    namespace Express {
+        interface Request {
+            user?: { _id: string };
+        }
+    }
+}
+
 export interface User {
     // TODO
     _id: UserId
