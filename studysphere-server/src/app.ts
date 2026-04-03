f39da400e3eee4431e5fedeb2f2afbeb4cd4aeb5
Aidan Ouckama <aidan@moonshot-ai.com>
2026-04-02 23:39:07 -0400

    add cors

--- studysphere-server/src/app.ts
+++ studysphere-server/src/app.ts
@@ -1,9 +1,16 @@
+import cors from "cors";
 import express from "express";
 import { port } from "./config/settings.js";
 import configRoutes from "./routes/index.js";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: process.env.CLIENT_ORIGIN ?? "http://localhost:3000",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
